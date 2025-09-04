bdc0111326d3f345a1d02afbd7734c424677ad39
nadhilkn <nadhunadhil33429@gmail.com>
2025-09-04 10:05:44 +0530

    fix

--- wedding card/vite.config.js
+++ wedding card/vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/ADNAN/",
+  base: process.env.VITE_BASE || "/ADNAN",
   build: {
     outDir: 'docs',
   },
